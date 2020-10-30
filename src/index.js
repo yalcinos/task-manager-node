90e6cd833b3bebeb8df57f67845678d70152ff41
yalcinos <yalcintatar41@gmail.com>
2020-10-29 21:06:44 -0700

    feat: Add User endpoints

--- src/index.js
+++ src/index.js
@@ -8,18 +8,18 @@ const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
-app.post("/users", (req, res) => {
+app.post("/users", async (req, res) => {
   const user = new User(req.body);
-  user
-    .save()
-    .then(() => {
-      res.status(201).send(user);
-    })
-    .catch((err) => {
-      res.status(400).send(err);
-    });
+  try {
+    //Everything will run after user.save function finish
+    await user.save();
+    res.status(201).send(user);
+  } catch (error) {
+    res.status(400).send(error);
+  }
 });
 
+//POST request for tasks
 app.post("/tasks", (req, res) => {
   const task = new Task(req.body);
   task
@@ -31,4 +31,60 @@ app.post("/tasks", (req, res) => {
       res.status(400).send(err);
     });
 });
+//Get all users
+app.get("/users", async (req, res) => {
+  try {
+    const users = await User.find({});
+    res.send(users);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+  //   User.find({})
+  //     .then((tasks) => {
+  //       res.status(200).send(tasks);
+  //     })
+  //     .catch((err) => {
+  //       res.status(500).send(err);
+  //     });
+});
+
+// Get User by ID
+app.get("/users/:id", async (req, res) => {
+  const id = req.params.id;
+
+  try {
+    const user = await User.findById(id);
+    if (!user) {
+      return res.status(404).send();
+    } else res.status(200).send(user);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+//Get all Tasks
+app.get("/tasks", async (req, res) => {
+  Task.find({})
+    .then((tasks) => {
+      res.status(200).send(tasks);
+    })
+    .catch((err) => {
+      res.status(500).send(err);
+    });
+});
+
+// Get Task by ID
+app.get("/tasks/:id", async (req, res) => {
+  const id = req.params.id;
+
+  try {
+    const task = await Task.findById(id);
+    if (!task) {
+      return res.status(404).send();
+    } else res.status(200).send(user);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
 app.listen(3000);
