4dc521bb7da1ecbc2854bbddf6ed55e73a931e86
yalcinos <yalcintatar41@gmail.com>
2020-10-29 18:57:34 -0700

    feat: User model is created

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,5 +1,5 @@
 const moongose = require("mongoose");
-const validator = require("validator");
+
 require("dotenv").config();
 moongose
   .connect(
@@ -13,42 +13,6 @@ moongose
     console.log(err);
   });
 
-//User modal
-const User = moongose.model("User", {
-  name: { type: String, required: true, trim: true },
-  email: {
-    type: String,
-    required: true,
-    trim: true,
-    lowercase: true,
-    validate(value) {
-      if (!validator.isEmail(value)) {
-        throw new Error("Email is not valid!");
-      }
-    },
-  },
-  password: {
-    type: String,
-    trim: true,
-    minlength: 7,
-    required: true,
-    validate(value) {
-      if (value.toLowerCase().includes("password")) {
-        throw new Error("Password cannot contain password.");
-      }
-    },
-  },
-  age: {
-    type: Number,
-    default: 0,
-    validate(value) {
-      if (value < 0) {
-        throw new Error("Age must be a positive number");
-      }
-    },
-  },
-});
-
 //Task modal
 const Task = moongose.model("Task", {
   description: {
@@ -61,17 +25,3 @@ const Task = moongose.model("Task", {
     default: false,
   },
 });
-
-const me = new User({
-  name: "Gokce",
-  email: "yalcin41@gmail.com",
-  password: "     dasdadasdassd    ",
-  age: 23,
-});
-me.save()
-  .then((data) => {
-    console.log(data);
-  })
-  .then((err) => {
-    console.log(err);
-  });
