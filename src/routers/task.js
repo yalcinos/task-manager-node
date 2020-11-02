eb71a6e7af2da66f2fbc60dd75dbc2294d10216f
yalcinos <yalcintatar41@gmail.com>
2020-11-02 13:38:58 -0800

    feat: change task patch method structure

--- src/routers/task.js
+++ src/routers/task.js
@@ -60,10 +60,11 @@ router.patch("/tasks/:id", async (req, res) => {
     res.status(400).send({ error: "Invalid updates" });
   }
   try {
-    const task = await Task.findByIdAndUpdate(id, req.body, {
-      new: true,
-      runValidators: true,
+    const task = await Task.findById(id);
+    updates.forEach((update) => {
+      task[update] = req.body[update];
     });
+    await task.save();
     if (!task) {
       return res.status(404).send({ error: "There is no task with this ID" });
     }
