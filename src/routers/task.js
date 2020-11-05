10a238ad544c9db4fa809877c7f4bc0395cd89c8
yalcinos <yalcintatar41@gmail.com>
2020-11-05 10:22:02 -0800

    Add filters to get task route

--- src/routers/task.js
+++ src/routers/task.js
@@ -23,9 +23,19 @@ router.post("/tasks", auth, async (req, res) => {
  * Get All tasks for a specific user
  */
 router.get("/tasks", auth, async (req, res) => {
+  const match = {};
+
+  if (req.query.completed) {
+    match.completed = req.query.completed === "true";
+  }
   try {
     //const tasks = await Task.find({ owner: req.user._id });
-    await req.user.populate("tasks").execPopulate();
+    await req.user
+      .populate({
+        path: "tasks",
+        match,
+      })
+      .execPopulate();
     res.send(req.user.tasks);
   } catch (error) {
     res.status(500).send(error);
