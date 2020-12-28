66e433fac1fe45ff8d9e5f2f1d43c7744a23b7d2
yalcinos <yalcintatar41@gmail.com>
2020-12-27 20:59:56 -0800

    refactor: Fix db connection url

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -2,10 +2,11 @@ const moongose = require("mongoose");
 
 require("dotenv").config();
 moongose
-  .connect(
-    `mongodb+srv://${process.env.DB_USERNAME}:${process.env.SERVER_PW}@${process.env.CLUSTER_NAME}.mq7d0.mongodb.net/Cluster0?retryWrites=true&w=majority`,
-    { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true }
-  )
+  .connect(process.env.MONGODB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  })
   .then(() => {
     console.log("MongoDB connected...");
   })
