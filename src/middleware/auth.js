f119ddbdacedfd8093e6261e0b3698c979e247a2
yalcinos <yalcintatar41@gmail.com>
2020-12-27 20:08:19 -0800

    feat: Update enviroment file

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,5 +1,6 @@
 const jwt = require("jsonwebtoken");
 const User = require("../models/user");
+require("dotenv").config();
 
 // With Middleware => new request => do something => run route handler
 // This middleware function helps to check client token and if it is valid return user informations
@@ -8,7 +9,7 @@ const auth = async (req, res, next) => {
     //get token from user.
     const token = req.header("Authorization").replace("Bearer ", "");
     //verift the user token
-    const decoded = jwt.verify(token, "token");
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
     // Check the token is same as in the server ?
     //Find the user with the correct id  who has that authentication token
     const user = await User.findOne({
--- src/models/user.js
+++ src/models/user.js
@@ -81,7 +81,7 @@ userSchema.methods.toJSON = function () {
 //Generate token and save the token to the DB. And send back to user.
 userSchema.methods.generateAuthToken = async function () {
   const user = this;
-  const token = jwt.sign({ _id: user._id.toString() }, "token");
+  const token = jwt.sign({ _id: user._id.toString() }, process.env.JWT_SECRET);
   user.tokens = user.tokens.concat({ token });
   await user.save();
   return token;
